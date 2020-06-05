2e38cbb22b563b18ce55c05188b9711ccd828f1f
[jose-martinez] <alias_one11@outlook.com>
2020-06-05 22:42:38 +0200

    Refactored: I refactored the app to Behavior Driven Development(BDD) format. The test are much clearer and concise!..

--- TDDBankAppTests/TDDBankAppTests.swift
+++ TDDBankAppTests/TDDBankAppTests.swift
@@ -3,13 +3,15 @@ import XCTest
 // modules into your unit testing project
 @testable import TDDBankApp
 
-class TDDBankAppTests: XCTestCase {
+// Base class for all the other test classes
+class Setup_Account: XCTestCase {
+    // MARK: #©Source-Of-Truth
+    var acc: Account!
 
-    internal var acc: Account!
-
-    // MARK: #©setUp will be run before each & every test
+    // MARK: _©Test XCTestCase
     /**©-----------------------©*/
     override func setUp() {
+        // Will run depending on how many test functions there are. 🤔
         printf("Created ()--> running func test")
         super.setUp()
         // Initializing our object
@@ -24,36 +26,43 @@ class TDDBankAppTests: XCTestCase {
         print("")
     }
     /**©-----------------------©*/
+}
 
+class BDD_when_creating_a_bank_account: Setup_Account {
     // MARK: _©Test-Funcs
     /**©------------------------------------------------------------------------------©*/
-
-    func test_WithdrawFromInsufficientFunds() {
-        // Test covers if you have insufficient
-        // funds or == 0 an error is thrown
-        acc.deposit(100)
-        XCTAssertThrowsError(try acc.withdraw(300)) { error in
-            XCTAssertEqual(error as! AccError, AccError.insufficientFunds)
-        }
+    func test_should_initialize_account_with_balance_zero() {
+        XCTAssertEqual(0, acc.balance)
     }
-    
-    func test_WithdrawFunds() {
+
+    /**©------------------------------------------------------------------------------©*/
+}
+
+class BDD_depositing_money_into_bank_account: Setup_Account {
+    // MARK: _©Test-Funcs
+    /**©------------------------------------------------------------------------------©*/
+    func test_should_deposit_successfully() {
+        acc.deposit(100)
+        XCTAssertEqual(100, acc.balance)
+    }
+    /**©------------------------------------------------------------------------------©*/
+}
+
+class BDD_withdrawing_funds_from_account: Setup_Account {
+    // MARK: _©Test-Funcs
+    /**©------------------------------------------------------------------------------©*/
+    func test_should_deduct_amount_successfully() {
         acc.deposit(100)
         try? acc.withdraw(50)
 
         XCTAssertEqual(50, acc.balance)
     }
-    func test_InitialBalanceZero() {
-        // Assert means that we either think the condition is true or false.
-        //Depending on which assert function we are implementing to check either.
-        printf("test_InitialBalanceZero()")
-        XCTAssertTrue(acc.balance == 0, "Balance is ! == zero")
-    }
 
-    func test_DepositFunds() {
-        printf("test_DepositFunds()")
-        acc.deposit(100)
-        XCTAssertEqual(100, acc.balance)
+    func test_should_throw_exception_when_there_is_insufficient_funds() {
+        acc.deposit(150)
+        XCTAssertThrowsError(try acc.withdraw(300)) { error in
+            XCTAssertEqual(error as? AccError, AccError.insufficientFunds)
+        }
     }
     /**©------------------------------------------------------------------------------©*/
 }
